9a3ee375b17fbf0e08e5ecbbbdb378a4e493f00d
crow <59679082+cr-0w@users.noreply.github.com>
2022-10-23 12:08:07 -0400

    Update nest.py
    v1.0.1 !

--- nest.py
+++ nest.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
 import os
 import sys
+import random
 import platform
+import argparse
 import netifaces
 import http.server
 from colorama import Fore, Style
 
-err = Fore.RED + '[X]' + Style.RESET_ALL
-okay = Fore.GREEN + '[+]' + Style.RESET_ALL
-info = Fore.YELLOW + '[!]' + Style.RESET_ALL
+# setup status colours
+err = Fore.LIGHTRED_EX + "[X]" + Style.RESET_ALL
+okay = Fore.LIGHTGREEN_EX + "[+]" + Style.RESET_ALL
+info = Fore.LIGHTYELLOW_EX + "[!]" + Style.RESET_ALL
 
+# check python3
 if sys.version_info.major != 3:
-    print('\n' + err + ' please run this script with python3!\n')
+    print("\n" + err, "please run this script with python3!\n")
     sys.exit(1)
-
-
+    
+# set func -> clear screen
 def clear():
-    if platform.system() == 'Linux':
-        os.system('clear')
+    if platform.system() == "Linux":
+        os.system("clear")
     else:
-        os.system('cls')
+        os.system("cls")
 
-
+# set func -> display banner
 def banner():
-    print(Fore.LIGHTYELLOW_EX + ''' \
-           ...    *    .   _  .   
+    print(
+        Fore.LIGHTYELLOW_EX
+        + """ \
+           ...    *    .   _  .
     *  .  *     .   * (_)   *
       .      |*  ..   *   ..
        .  * \|  *  ___  . . *
     *   \/   |/ \/{o,o}     .
-      _\_\   |  / /)  )* _/_ *      nest! (simple http server) v1.0.0
+      _\_\   |  / /)  )* _/_ *      nest! (simple http server) v1.0.1
           \ \| /,--"-"---  ..                   by cr0w
     _-----`  |(,__,__/__/_ .
            \ ||      ..
@@ -37,79 +43,350 @@ def banner():
             |||
             |||
       , -=-~' .-^- _
-    ''', Style.RESET_ALL)
+    """,
+        Style.RESET_ALL,
+    )
 
-
+# setup handler and argparse + globals
 Handler = http.server.SimpleHTTPRequestHandler
 
+interfaces = netifaces.interfaces()
 
+parser = argparse.ArgumentParser(description="a (better) simple http server!")
+parser.add_argument(
+    "-i",
+    "--interactive",
+    dest="interactive",
+    default=False,
+    required=False,
+    action="store_true",
+    help="interactively setup the server",
+)
+parser.add_argument(
+    "-c",
+    "--ctf",
+    dest="ctf",
+    default=False,
+    required=False,
+    action="store_true",
+    help='if you select this, nest will look for common CTF interfaces (i.e., "tun0" or "tap0") and automatically start serving on that interface on port 8080.',
+)
+parser.add_argument(
+    "-v",
+    "--version",
+    help="prints version.",
+    action="version",
+    version="%(prog)s v1.0.1",
+)
+
+args = parser.parse_args()
+
+# show example
+def show_example():
+    tot_ifaces = len(interfaces)
+    ran_iface = random.randrange(0, tot_ifaces)
+    fin_iface = ran_iface + 1
+    print(
+        info,
+        "for example >>> to select",
+        Fore.LIGHTGREEN_EX + "{}".format(interfaces[ran_iface]) + Style.RESET_ALL + ",",
+        "enter" + Fore.LIGHTGREEN_EX,
+        fin_iface,
+        Style.RESET_ALL,
+    )
+
+# enumerate network interfaces
 def enum_interfaces():
-    print(okay, 'welcome to the', Fore.YELLOW + 'nest!')
-    print(info, 'gathering interfaces...')
-    interface_list = netifaces.interfaces()
-    print(info, 'got', Fore.GREEN + '{}'.format(len(interface_list)), Style.RESET_ALL + 'interfaces')
-    print(okay, 'here they are!', Fore.GREEN + '{}'.format(', '.join(interface_list)), Style.RESET_ALL)
-    print(info, 'please select an interface to serve on -> choose:' + Fore.LIGHTGREEN_EX,
-          '1-{}'.format(len(interface_list)),
-          Style.RESET_ALL, Fore.RED)
+    print(info, "collecting interfaces on the local machine...")
+    print(
+        okay,
+        "finished. found" + Fore.LIGHTGREEN_EX,
+        "{}".format(len(interfaces)),
+        Style.RESET_ALL + "interface(s)",
+    )
+    print(
+        info,
+        "here they are:",
+        Fore.GREEN + "{}".format(", ".join(interfaces), Style.RESET_ALL),
+    )
 
+# select interface
+def select_interface():
+    print(
+        info,
+        "please select an interface to serve on ---> choose:" + Fore.LIGHTGREEN_EX,
+        "1-{}".format(len(interfaces)),
+        Style.RESET_ALL,
+    )
+    show_example()
+    print(Fore.LIGHTYELLOW_EX)
     try:
-        element = input('\n\rselect an interface: ')
-
-        if element.isdigit():
-            element = int(element)
-            if element > len(interface_list):
-                print('\n\r' + Style.RESET_ALL + err, 'there aren\'t that many interfaces. use a valid number.\n')
-            else:
-                index = int(element) - 1
-                print('\n\r' + okay, 'selected:' + Fore.GREEN, '{}'.format(interface_list[index]), Style.RESET_ALL)
-                interface = interface_list[index]
-                af_inet = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
-                print(okay, Fore.YELLOW + '{}'.format(interface_list[index]), Style.RESET_ALL +
-                      '--->' + Fore.GREEN, '{}'.format(af_inet), Style.RESET_ALL, '\n')
-                af_inet = str(af_inet)
-
-                print(info, 'getting port...')
-                print(info, 'please note that if you supply a port number' + Fore.RED,
-                      'below 1024' + Style.RESET_ALL + ', you need to run the '
-                                                       'program as' + Fore.RED, 'root',
-                      Style.RESET_ALL + 'since the first 1024 ports '
-                                        'are reserved for the root '
-                                        'user.', Style.RESET_ALL, Fore.RED)
-                port = input('\n\rselect a port for hosting: ')
-                if port.isdigit():
-                    port = int(port)
-                    print('\n\r' + Style.RESET_ALL + okay, 'got port:' + Fore.GREEN, '{}'.format(port), Style.RESET_ALL)
-
-                    print(info, 'starting to serve...')
-                    host_address = (af_inet, port)
-                    print(info, 'all set!')
-                    server = http.server.HTTPServer(host_address, http.server.SimpleHTTPRequestHandler)
-                    print(okay, 'serving at -->' + Fore.GREEN, 'http://{}:{}'.format(af_inet, port), Style.RESET_ALL,
-                          '\n')
-
-                    print(info, 'listing files in current directory for QoL...\n', Fore.YELLOW)
-                    for root, dirs, files in os.walk('.', topdown=True):
-                        dirs.clear()  # with topdown true, this will prevent walk from going into subs
-                        for file in files:
-                            print("http://{}:{}/{}".format(af_inet, port, file))
-                        print(Style.RESET_ALL, Fore.LIGHTGREEN_EX)
-                    server.serve_forever()
-
-                else:
-                    print('\n\r' + err, 'please supply a' + Fore.RED, 'number', Style.RESET_ALL + 'for the port.\n')
-                    sys.exit(1)
-
+        selection = input("\rnest >> ")
+        if selection.isdigit():
+            selection = int(selection)
         else:
-            print('\n\r' + err, 'please supply a number that corresponds to an interface. ')
-            print(info, 'example: to select', Fore.GREEN + 'eth0' + Style.RESET_ALL + ',', 'input', Fore.GREEN + '2.\n')
-
+            print(err, "non-digit supplied. please enter in a valid number.")
+            sys.exit(1)
     except KeyboardInterrupt:
-        print("\n\n\r" + err, 'caught exception. exiting...\n')
+        print(
+            "\n\n" + err,
+            "caught" + Fore.LIGHTRED_EX,
+            "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+        )
         sys.exit(1)
+    if selection > (len(interfaces)):
+        print(
+            "\n\r" + err,
+            "there aren't that many interfaces on this machine. select a valid number.",
+        )
+    else:
+        index = int(selection) - 1
+        interface = interfaces[index]
+        af_inet = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]["addr"]
+        print(
+            "\n" + okay,
+            "interface selected:" + Fore.LIGHTGREEN_EX,
+            "{}".format(interface),
+            Style.RESET_ALL
+            + Fore.LIGHTYELLOW_EX
+            + "///// "
+            + Fore.LIGHTGREEN_EX
+            + "{}".format(af_inet),
+        )
+        ip = str(af_inet)
 
+        # get the port
+        print(info, "getting server port...")
+        print(info, "please select a port for hosting", Fore.LIGHTYELLOW_EX)
+        try:
+            port = input("\n\rnest >> ")
+            if port.isdigit():
+                port = int(port)
+                host = (ip, port)
+                print(
+                    "\n" + info,
+                    "populated host parameters",
+                    Fore.LIGHTYELLOW_EX + "/////",
+                    Fore.LIGHTGREEN_EX + "{}:{}".format(ip, port),
+                )
+                print(okay, "starting to serve...")
+                
+            # introduce QoL stuff here before serving
+            print(info, "listing files for QoL...\n")
+            for path, subdirs, files in os.walk("."):
+                for name in files:
+                    print(
+                        Fore.LIGHTYELLOW_EX
+                        + "http://{}:{}/{}".format(
+                            af_inet, port, os.path.join(path.strip("./"), name)
+                        )
+                        + Style.RESET_ALL
+                    )
+            print(
+                "\n" + okay,
+                "server started @" + Fore.LIGHTGREEN_EX,
+                "http://{}:{}".format(af_inet, port),
+                Style.RESET_ALL + "\n" + Fore.LIGHTGREEN_EX,
+            )
+            try:
+                server = http.server.HTTPServer(
+                    host, http.server.SimpleHTTPRequestHandler
+                )
+                server.serve_forever()
+            except KeyboardInterrupt:
+                print(
+                    "\n\n" + err,
+                    "caught" + Fore.LIGHTRED_EX,
+                    "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+                )
+                sys.exit(1)
+            else:
+                print("\n" + err, "the port isn't a number. exiting.")
+                sys.exit(1)
+        except KeyboardInterrupt:
+            print(
+                "\n\n" + err,
+                "caught" + Fore.LIGHTRED_EX,
+                "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+            )
+            sys.exit(1)
 
-if __name__ == "__main__":
+# if --ctf/-c is supplied, search for common CTF NICs and serve on them in the current directory
+if args.ctf:
     clear()
     banner()
+    print(okay, "ctf quick toggle enabled.")
+    print(
+        info,
+        "searching for" + Fore.LIGHTYELLOW_EX,
+        "tun0/tap0",
+        Style.RESET_ALL + "on the local machine.",
+    )
+    ctf_nics = ["tun0", "tap0"]
+
+    if ctf_nics[0] in interfaces:
+        port = 8080
+        ip = netifaces.ifaddresses("tun0")[netifaces.AF_INET][0]["addr"]
+        print(
+            okay,
+            "found interface! >>>" + Fore.LIGHTGREEN_EX,
+            "{}".format(ctf_nics[0]),
+            Fore.LIGHTYELLOW_EX + "/////" + Fore.LIGHTGREEN_EX,
+            "{}".format(ip),
+        )
+        print(okay, "default port set >>>", Fore.LIGHTGREEN_EX + "{}".format(port))
+        print(
+            info,
+            "populated host parameters",
+            Fore.LIGHTYELLOW_EX + "/////",
+            Fore.LIGHTGREEN_EX + "{}:{}".format(ip, port),
+        )
+        host = (ip, port)
+        print(info, "listing files for QoL...\n")
+        for path, subdirs, files in os.walk("."):
+            for name in files:
+                print(
+                    Fore.LIGHTYELLOW_EX
+                    + "http://{}:{}/{}".format(
+                        ip, port, os.path.join(path.strip("./"), name)
+                    )
+                    + Style.RESET_ALL
+                )
+        print(
+            "\n" + okay,
+            "server started @" + Fore.LIGHTGREEN_EX,
+            "http://{}:{}".format(ip, port),
+            Style.RESET_ALL + "\n" + Fore.LIGHTGREEN_EX,
+        )
+        try:
+            server = http.server.HTTPServer(host, http.server.SimpleHTTPRequestHandler)
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print(
+                "\n\n" + err,
+                "caught" + Fore.LIGHTRED_EX,
+                "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+            )
+            sys.exit(1)
+    elif ctf_nics[1] in interfaces:
+        clear()
+        banner()
+        print(okay, "ctf quick toggle enabled")
+        print(
+            info,
+            "searching for" + Fore.LIGHTYELLOW_EX,
+            "tun0/tap0",
+            Style.RESET_ALL + "on the local machine.",
+        )
+        print(
+            okay, "found interface! >>>" + Fore.LIGHTGREEN_EX, "{}".format(ctf_nics[1])
+        )
+        port = 8080
+        ip = netifaces.ifaddresses("tap0")[netifaces.AF_INET][0]["addr"]
+        print(
+            okay,
+            "found interface! >>>" + Fore.LIGHTGREEN_EX,
+            "{}".format(ctf_nics[0]),
+            Fore.LIGHTYELLOW_EX + "/////" + Fore.LIGHTGREEN_EX,
+            "{}".format(ip),
+        )
+        print(okay, "default port set >>>", Fore.LIGHTGREEN_EX + "{}".format(port))
+        print(
+            info,
+            "populated host parameters",
+            Fore.LIGHTYELLOW_EX + "/////",
+            Fore.LIGHTGREEN_EX + "{}:{}".format(ip, port),
+        )
+        host = (ip, port)
+        print(info, "listing files for QoL...\n")
+        for path, subdirs, files in os.walk("."):
+            for name in files:
+                print(
+                    Fore.LIGHTYELLOW_EX
+                    + "http://{}:{}/{}".format(
+                        ip, port, os.path.join(path.strip("./"), name)
+                    )
+                    + Style.RESET_ALL
+                )
+        print(
+            "\n" + okay,
+            "server started @" + Fore.LIGHTGREEN_EX,
+            "http://{}:{}".format(ip, port),
+            Style.RESET_ALL + "\n" + Fore.LIGHTGREEN_EX,
+        )
+        try:
+            server = http.server.HTTPServer(host, http.server.SimpleHTTPRequestHandler)
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print(
+                "\n\n" + err,
+                "caught" + Fore.LIGHTRED_EX,
+                "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+            )
+            sys.exit(1)
+    else:
+        print(err, "no ctf interface found.\n")
+        sys.exit(1)
+        
+# unless specified with --interactive/-i, run using the default settings -> specified in argparse.
+if args.interactive:
+    clear()
+    banner()
+    print(info, "interactive switch toggled.")
     enum_interfaces()
+    select_interface()
+else:
+    clear()
+    banner()
+    all_ifaces = "0.0.0.0"
+    port = 8080
+    print(info, "no args supplied, defaulting to quick toggle")
+    print(
+        info,
+        "will serve on all interfaces:" + Fore.LIGHTYELLOW_EX,
+        "{}".format(all_ifaces),
+    )
+    print(info, "default port set:" + Fore.LIGHTYELLOW_EX, "8080")
+    host = (all_ifaces, port)
+    print()
+
+    for iface in interfaces:
+        print(
+            okay,
+            "serving on >>>" + Fore.LIGHTGREEN_EX,
+            "{}".format(iface),
+            Style.RESET_ALL,
+        )
+    print("\n" + info, "listing files for QoL...")
+    for path, subdirs, files in os.walk("."):
+        for iface in interfaces:
+            print()
+            ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
+            for name in files:
+                print(
+                    Fore.LIGHTYELLOW_EX
+                    + "http://{}:{}/{}".format(
+                        ip, port, os.path.join(path.strip("./"), name)
+                    )
+                    + Style.RESET_ALL
+                )
+    print()
+    for iface in interfaces:
+        ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
+        print(
+            okay,
+            "server started @" + Fore.LIGHTGREEN_EX,
+            "http://{}:{}".format(iface, port) + Fore.LIGHTYELLOW_EX,
+            "---> http://{}:{}".format(ip, port),
+            Style.RESET_ALL + Fore.LIGHTGREEN_EX,
+        )
+    print()
+    try:
+        server = http.server.HTTPServer(host, http.server.SimpleHTTPRequestHandler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print(
+            "\n\n" + err,
+            "caught" + Fore.LIGHTRED_EX,
+            "KeyboardInterrupt" + Style.RESET_ALL + ". exiting...\n",
+        )
